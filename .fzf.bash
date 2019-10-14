3e5e09b7086e26542eba8bc6398927d48a54291e
Carles Rafols i Belles <c.rafols-i-belles16@imperial.ac.uk>
2019-10-14 10:25:56 +0100

    Adding fzf.bash

---
+++ .fzf.bash
@@ -0,0 +1,33 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/usr/local/opt/fzf/shell/key-bindings.bash"
+
+# Overwriting default commands and options
+# ---------------------
+export FZF_CTRL_T_COMMAND='fd --type f --hidden --no-ignore --exclude .git --max-depth 4'
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --exclude .git --max-depth 4'
+export FZF_DEFAULT_OPTS="--exact --layout=reverse --inline-info"
+
+# Overwriting default functions
+# ---------------------
+_fzf_compgen_path() {
+  fd --type f --hidden --no-ignore --exclude ".git" --max-depth 4 . "$1"
+}
+
+_fzf_compgen_dir() {
+  fd --type d --hidden --no-ignore --exclude ".git" --max-depth 4 . "$1"
+}
+
+# Adding more commands for completion
+# ---------------------
+complete -F _fzf_path_completion -o default -o bashdefault v
