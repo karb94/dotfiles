03a761a998a95d588288be0e88b529208ef363e9
karb94 <c.rafols-i-belles16@imperial.ac.uk>
2019-10-24 18:37:20 +0100

    Updated dotfiles

--- .fzf.bash
+++ .fzf.bash
@@ -14,19 +14,25 @@ source "$HOME/.fzf/shell/key-bindings.bash"
 
 # Overwriting default commands and options
 # ---------------------
-export FZF_CTRL_T_COMMAND='fd --type f --hidden --no-ignore --exclude .git --max-depth 3'
-export FZF_DEFAULT_COMMAND='fd --type f --hidden --exclude .git --max-depth 3'
+if [[ ! $HOSTNAME =~ login-[0-9]+ ]]
+then
+    export FZF_CTRL_T_COMMAND='fd --type f --hidden --no-ignore --exclude .git --max-depth 3'
+    export FZF_DEFAULT_COMMAND='fd --type f --hidden --exclude .git --max-depth 3'
+fi
 export FZF_DEFAULT_OPTS="--exact --layout=reverse --inline-info"
 
 # Overwriting default functions
 # ---------------------
-_fzf_compgen_path() {
-  fd --type f --hidden --no-ignore --exclude ".git" --max-depth 3 . "$1"
-}
+if [[ ! $HOSTNAME =~ login-[0-9]+ ]]
+then
+    _fzf_compgen_path() {
+      fd --type f --hidden --no-ignore --exclude ".git" --max-depth 3 . "$1"
+    }
 
-_fzf_compgen_dir() {
-  fd --type d --hidden --no-ignore --exclude ".git" --max-depth 3 . "$1"
-}
+    _fzf_compgen_dir() {
+      fd --type d --hidden --no-ignore --exclude ".git" --max-depth 3 . "$1"
+    }
+fi
 
 # Adding more commands for completion
 # ---------------------
