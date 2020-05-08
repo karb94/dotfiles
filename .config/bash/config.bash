f896ec7ffceea9066c850c26e747b75456f7af8c
karb94 <c.rafols-i-belles16@imperial.ac.uk>
2020-05-08 13:21:55 +0100

    fast push

--- .config/bash/config.bash
+++ .config/bash/config.bash
@@ -18,15 +18,17 @@ shopt -s extglob
 # Define your command prompt
 blue1='\033[38;5;4m'
 blue2='\033[38;5;12m'
+red1='\033[38;5;6m'
 yellow2='\033[38;5;11m'
 green2='\033[38;5;14m'
 bold='\033[1m'          # Makes following text bold
 reset_font='\033[0m'    # Applies default font attributes
 ssh_prompt () {
-    [ -n $SSH_CONNECTION ] && printf "${yellow2}\h" 
+    [ -n "$SSH_CONNECTION" ] &&
+        printf "${yellow2}${HOSTNAME}${reset_font}" 
 }
 PS1="
-\[${ssh_prompt}${blue1}\]\u:\[${blue2}\]\w/
+\[$(ssh_prompt)${blue1}\]\u:\[${blue2}\]\w/
  \[${bold}${green2}\]>\[${reset_font}\] "
 
 # Prompt command
