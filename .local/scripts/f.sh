70885167cd5a8034394a9e0f2a7a68403ddabd41
Carles Rafols <41967813+karb94@users.noreply.github.com>
2023-04-16 18:55:55 +0000

    Fast push

--- .local/scripts/f.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-if [ -z "$@" ]
-then
-    find ~/phd/literature -printf '%P\n'
-    # echo "ROFI_RETV = ${ROFI_RETV}"
-else
-    coproc ( zathura ~/phd/literature/"$@" & > /dev/null 2>&1 )
-fi
