b910e8ba0df475a949637de960031d3c9389c67c
Carles Rafols <41967813+karb94@users.noreply.github.com>
2023-12-08 13:53:34 +0000

    Fast push

--- .config/nvim/lua/plugins/gruvbox.lua
+++ .config/nvim/lua/plugins/gruvbox.lua
@@ -1,14 +1,9 @@
---vim.g.gruvbox_contrast_dark = 'hard'
---vim.g.gruvbox_contrast_light = 'soft'
---vim.g.gruvbox_sign_column = 'bg0'
---vim.cmd('colorscheme gruvbox')
-
 return {
   "ellisonleao/gruvbox.nvim",
   priority=1000,
   opts = function()
-    local colors = require("gruvbox.palette").colors
-    local dark0_hard_bg = {bg = colors.dark0_hard}
+    local palette = require("gruvbox").palette
+    local dark0_hard_bg = {bg = palette.dark0_hard}
     return {
       inverse = false,
       contrast = "hard",
@@ -20,8 +15,8 @@ return {
         GruvboxPurpleSign = dark0_hard_bg,
         GruvboxAquaSign = dark0_hard_bg,
         GruvboxOrangeSign = dark0_hard_bg,
-        CursorLine = {bg = colors.dark0},
-        CursorLineNr = {bg = colors.dark0_hard, bold = true}
+        CursorLine = {bg = palette.dark0},
+        CursorLineNr = {bg = palette.dark0_hard, bold = true}
       },
     }
   end,
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -4,10 +4,16 @@ return {
   -- {dir = "~/projects/neoscroll.nvim", opts = {}},
   {dir = "~/projects/neoscroll.nvim", opts = {stop_eof = true}},
   -- {dir = "~/projects/neoscroll.nvim", opts = {stop_eof = false}},
-  {"lukas-reineke/indent-blankline.nvim", opts = {char = '┊', show_current_context = false}},
+  {"lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {indent = {char = '┊'}}},
   {"norcalli/nvim-colorizer.lua", config = true},
   {"lewis6991/gitsigns.nvim", config = true},
-  {"chaoren/vim-wordmotion"},
-  {"ggandor/flit.nvim"},
+  {"chaoren/vim-wordmotion", init=function ()
+    vim.g.wordmotion_uppercase_spaces = {
+      '(', ')', '[', ']' , '{', '}',
+      '.', '-', ',', '/', '"', "'"
+    }
+  end},
+  {"stevearc/oil.nvim", opts={}, dependencies={"nvim-tree/nvim-web-devicons"}},
+  {"ggandor/flit.nvim", opts={}},
   {"nvim-lua/plenary.nvim"},
 }
